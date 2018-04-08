95e3afd30ff6b501761d71d85b866e495e277cc5
Stuart Taylor <staylor1012@gmail.com>
2018-04-08 23:13:11 +0100

    Move templates out of the veiws/ directory

---
+++ src/templates/Challenges/components/Output.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import MonacoEditor from 'react-monaco-editor';
+
+const propTypes = {
+  defaultOutput: PropTypes.string,
+  output: PropTypes.string
+};
+
+const options = {
+  lineNumbers: false,
+  minimap: {
+    enabled: false
+  },
+  readOnly: true,
+  scrollbar: {
+    vertical: 'hidden',
+    horizontal: 'hidden'
+  },
+  wordWrap: 'on'
+};
+
+function Output({ output, defaultOutput }) {
+  return (
+    <Fragment>
+      <base href='/' />
+      <MonacoEditor
+        className='challenge-output'
+        height={150}
+        options={options}
+        value={output ? output : defaultOutput}
+      />
+    </Fragment>
+  );
+}
+
+Output.displayName = 'Output';
+Output.propTypes = propTypes;
+
+export default Output;
