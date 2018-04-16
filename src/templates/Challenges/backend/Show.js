dbdac80114c71aba0370ff9e8c66522f4f13b9eb
Stuart Taylor <staylor1012@gmail.com>
2018-04-16 11:53:58 +0100

    Add backend view

--- src/templates/Challenges/backend/Show.js
+++ src/templates/Challenges/backend/Show.js
@@ -1,12 +1,9 @@
+/* global graphql */
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { createSelector } from 'reselect';
 import { reduxForm } from 'redux-form';
-import {
-  Button,
-  Col,
-  Row
-} from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 import ChallengeTitle from '../components/Challenge-Title';
 import ChallengeDescription from '../components/Challenge-Description';
@@ -14,17 +11,18 @@ import TestSuite from '../components/Test-Suite';
 import Output from '../components/Output';
 import {
   executeChallenge,
-  testsSelector,
-  outputSelector
+  challengeTestsSelector,
+  consoleOutputSelector,
+  initTests,
+  updateChallengeMeta
 } from '../redux';
-import { descriptionRegex } from '../utils';
 
 import {
   createFormValidator,
   isValidURL,
-  makeRequired
-} from '../../../../utils/form.js';
-import { challengeSelector } from '../../../../redux';
+  makeRequired,
+  Form
+} from '../../../components/formHelpers';
 
 // provided by redux form
 const reduxFormPropTypes = {
@@ -44,95 +42,108 @@ const propTypes = {
   ...reduxFormPropTypes
 };
 
-const fields = [ 'solution' ];
+const fields = ['solution'];
 
 const fieldValidators = {
   solution: makeRequired(isValidURL)
 };
 
 const mapStateToProps = createSelector(
-  challengeSelector,
-  outputSelector,
-  testsSelector,
-  (
-    {
-      id,
-      title,
-      description
-    },
-    output,
-    tests
-  ) => ({
-    id,
-    title,
+  consoleOutputSelector,
+  challengeTestsSelector,
+  (output, tests) => ({
     tests,
-    description,
     output
   })
 );
 
 const mapDispatchToActions = {
-  executeChallenge
+  executeChallenge,
+  initTests,
+  updateChallengeMeta
+};
+
+const formFields = ['solution'];
+const options = {
+  required: ['solution'],
+  types: {
+    solution: 'url'
+  }
 };
 
 export class BackEnd extends PureComponent {
+  constructor(...props) {
+    super(...props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    const {
+      initTests,
+      updateChallengeMeta,
+      data: { challengeNode: { fields: { tests } } },
+      pathContext: { challengeMeta }
+    } = this.props;
+    initTests(tests);
+    updateChallengeMeta(challengeMeta);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { data: { challengeNode: { title: prevTitle } } } = prevProps;
+    const {
+      initTests,
+      updateChallengeMeta,
+      data: { challengeNode: { title: currentTitle, fields: { tests } } },
+      pathContext: { challengeMeta }
+    } = this.props;
+    if (prevTitle !== currentTitle) {
+      initTests(tests);
+      updateChallengeMeta(challengeMeta);
+    }
+  }
+  handleSubmit(values) {
+    console.log('backend', values);
+  }
+
   render() {
     const {
-      description,
-      executeChallenge,
+      data: { challengeNode: { fields: { blockName }, title, description } },
       output,
       tests,
-      title,
-      // provided by redux-form
-      fields: { solution },
-      handleSubmit,
       submitting
     } = this.props;
 
-    const buttonCopy = submitting ?
-      'Submit and go to my next challenge' :
-      "I've completed this challenge";
+    const buttonCopy = submitting
+      ? 'Submit and go to my next challenge'
+      : "I've completed this challenge";
+    const blockNameTitle = `${blockName} - ${title}`;
     return (
       <Row>
-        <Col
-          xs={ 6 }
-          xsOffset={ 3 }
-          >
+        <Col xs={6} xsOffset={3}>
           <Row>
-            <ChallengeTitle>
-              { title }
-            </ChallengeTitle>
+            <ChallengeTitle>{blockNameTitle}</ChallengeTitle>
             <ChallengeDescription description={description} />
           </Row>
           <Row>
-            <form
-              name='BackEndChallenge'
-              onSubmit={ handleSubmit(executeChallenge) }
-              >
-              <Button
-                block={ true }
-                bsStyle='primary'
-                className='btn-big'
-                onClick={ submitting ? null : null }
-                type={ submitting ? null : 'submit' }
-                >
-                { buttonCopy } (ctrl + enter)
-              </Button>
-            </form>
-          </Row>
-          <Row>
-            <br/>
-            <Output
-              defaultOutput={
-`/**
-  * Test output will go here
-  */`
-              }
-              output={ output }
+            <Form
+              buttonText={buttonCopy + '(Ctrl + Enter)'}
+              formFields={formFields}
+              id='backend-form'
+              options={options}
+              submit={this.handleSubmit}
             />
           </Row>
           <Row>
-            <TestSuite tests={ tests } />
+            <br />
+            <Output
+              defaultOutput={`/**
+  * Test output will go here
+  */`}
+              output={output}
+            />
+          </Row>
+          <Row>
+            <TestSuite tests={tests} />
           </Row>
         </Col>
       </Row>
@@ -152,3 +163,21 @@ export default reduxForm(
   mapStateToProps,
   mapDispatchToActions
 )(BackEnd);
+
+export const query = graphql`
+  query BackendChallenge($slug: String!) {
+    challengeNode(fields: { slug: { eq: $slug } }) {
+      title
+      guideUrl
+      description
+      challengeType
+      fields {
+        blockName
+        tests {
+          text
+          testString
+        }
+      }
+    }
+  }
+`;
