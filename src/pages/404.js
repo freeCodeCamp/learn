0785fcdc99a01c48ed834cf3fd371ded1eedd43a
Jrom <jromestiller@gmail.com>
2018-06-24 07:24:44 +0800

    Modify 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
+import './404.css';
+import notFoundLogo from '../../static/img/freeCodeCamp-404.svg';
 
 const NotFoundPage = () => (
-  <div>
+  <div className='notfound-page-wrapper'>
+    <Helmet title='Page Not Found | freeCodeCamp' />
+    <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
