12eef0f93924c5c343cc8bf16860d6b1c2ef79c7
Wadinga Leonard <wadingaleonard@gmail.com>
2018-08-10 15:51:04 +0100

    fix random page flickering on page load #256

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,26 +8,29 @@ import { quotes } from '../../static/json/quotes.json';
 
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-const NotFoundPage = () => (
-  <div className='notfound-page-wrapper'>
-    <Helmet title='Page Not Found | freeCodeCamp' />
+class NotFoundPage extends React.PureComponent {
+  render() {
+    return (
+      <div className='notfound-page-wrapper'>
+        <Helmet title='Page Not Found | freeCodeCamp' />
 
-    <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
-    <h1>NOT FOUND</h1>
-    <p>We couldn't find what you were looking for, but here is a quote:</p>
+        <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
+        <h1>NOT FOUND</h1>
+        <p>We couldn't find what you were looking for, but here is a quote:</p>
+        <div className='quote-wrapper'>
+          <p className='quote'>
+            <span>&#8220;</span>
+            {randomQuote.quote}
+          </p>
+          <p className='author'>- {randomQuote.author}</p>
+        </div>
+        <Link className='btn-curriculum' to='/'>
+          View the Curriculum
+        </Link>
+      </div>
+    );
+  }
+}
 
-    <div className='quote-wrapper'>
-      <p className='quote'>
-        <span>&#8220;</span>
-        {randomQuote.quote}
-      </p>
-      <p className='author'>- {randomQuote.author}</p>
-    </div>
-
-    <Link className='btn-curriculum' to='/'>
-      View the Curriculum
-    </Link>
-  </div>
-);
 
 export default NotFoundPage;
