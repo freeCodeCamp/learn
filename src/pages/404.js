9a1eac2e065cc23ff7f9f8a352aad44e9417e18c
Wadinga Leonard <wadingaleonard@gmail.com>
2018-09-12 11:28:00 +0100

    added spinner when quote has not yet loaded

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,36 +6,55 @@ import './404.css';
 import notFoundLogo from '../../static/img/freeCodeCamp-404.svg';
 import { quotes } from '../../static/json/quotes.json';
 
-let randomQuote: any = {};
+var Spinner = require('react-spinkit');
+let randomQuote: any;
 
 class NotFoundPage extends React.Component {
-
   componentDidMount() {
     randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   }
 
   render() {
-    return (
-      <div className='notfound-page-wrapper'>
-        <Helmet title='Page Not Found | freeCodeCamp' />
+    if (randomQuote !== undefined) {
+      return (
+        <div className='notfound-page-wrapper'>
+          <Helmet title='Page Not Found | freeCodeCamp' />
 
-        <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
-        <h1>NOT FOUND</h1>
-        <p>We couldn't find what you were looking for, but here is a quote:</p>
-        <div className='quote-wrapper'>
-          <p className='quote'>
-            <span>&#8220;</span>
-            {randomQuote.quote}
+          <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
+          <h1>NOT FOUND</h1>
+          <p>
+            We couldn&#x27;t find what you were looking for, but here is a quote:
           </p>
-          <p className='author'>- {randomQuote.author}</p>
+          <div className='quote-wrapper'>
+            <p className='quote'>
+              <span>&#8220;</span>
+              {randomQuote.quote}
+            </p>
+            <p className='author'>- {randomQuote.author}</p>
+          </div>
+          <Link className='btn-curriculum' to='/'>
+            View the Curriculum
+          </Link>
         </div>
-        <Link className='btn-curriculum' to='/'>
-          View the Curriculum
-        </Link>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className='notfound-page-wrapper'>
+          <Helmet title='Page Not Found | freeCodeCamp' />
+
+          <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
+          <h1>NOT FOUND</h1>
+          <p>
+            We couldn&#x27;t find what you were looking for, but here is a quote:
+          </p>
+          <Spinner color='#006400' name='ball-clip-rotate-multiple' />
+          <Link className='btn-curriculum' to='/'>
+            View the Curriculum
+          </Link>
+        </div>
+      );
+    }
   }
 }
-
 
 export default NotFoundPage;
