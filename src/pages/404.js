8b219d0bdac5edd3752089197e262eafb6976efc
Wadinga Leonard <wadingaleonard@gmail.com>
2018-09-16 18:49:59 +0100

    use better standards

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,59 +1,60 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
+import Spinner from 'react-spinkit';
 
 import './404.css';
 import notFoundLogo from '../../static/img/freeCodeCamp-404.svg';
 import { quotes } from '../../static/json/quotes.json';
 
-var Spinner = require('react-spinkit');
-let randomQuote: any;
-
 class NotFoundPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomQuote: null
+    }
+  }
+
   componentDidMount() {
-    randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    this.updateQuote();
+  }
+
+  updateQuote() {
+    this.setState({
+      randomQuote: quotes[Math.floor(Math.random() * quotes.length)]
+    });
   }
 
   render() {
-    if (randomQuote !== undefined) {
-      return (
-        <div className='notfound-page-wrapper'>
-          <Helmet title='Page Not Found | freeCodeCamp' />
-
-          <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
-          <h1>NOT FOUND</h1>
-          <p>
-            We couldn&#x27;t find what you were looking for, but here is a quote:
-          </p>
-          <div className='quote-wrapper'>
-            <p className='quote'>
-              <span>&#8220;</span>
-              {randomQuote.quote}
-            </p>
-            <p className='author'>- {randomQuote.author}</p>
-          </div>
-          <Link className='btn-curriculum' to='/'>
-            View the Curriculum
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <div className='notfound-page-wrapper'>
-          <Helmet title='Page Not Found | freeCodeCamp' />
-
-          <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
-          <h1>NOT FOUND</h1>
-          <p>
-            We couldn&#x27;t find what you were looking for, but here is a quote:
-          </p>
-          <Spinner color='#006400' name='ball-clip-rotate-multiple' />
-          <Link className='btn-curriculum' to='/'>
-            View the Curriculum
-          </Link>
-        </div>
-      );
-    }
+    return (
+      <div className='notfound-page-wrapper'>
+        <Helmet title='Page Not Found | freeCodeCamp' />
+        <img alt='learn to code at freeCodeCamp 404' src={notFoundLogo} />
+        <h1>NOT FOUND</h1>
+        {
+          this.state.randomQuote ? (
+            <div>
+              <p>
+                We couldn&#x27;t find what you were looking for, but here is a
+                quote:
+              </p>
+              <div className='quote-wrapper'>
+                <p className='quote'>
+                  <span>&#8220;</span>
+                  {this.state.randomQuote.quote}
+                </p>
+                <p className='author'>- {this.state.randomQuote.author}</p>
+              </div>
+            </div>
+          ) : (
+            <Spinner color='#006400' name='ball-clip-rotate-multiple' />
+          )
+        }
+        <Link className='btn-curriculum' to='/'>
+          View the Curriculum
+        </Link>
+      </div>
+    );
   }
 }
 
