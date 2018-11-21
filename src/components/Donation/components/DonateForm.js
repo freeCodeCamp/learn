accba2a85094f46bd30b63503cf048cc891f7bd3
mrugesh mohapatra <1884376+raisedadead@users.noreply.github.com>
2018-11-21 19:27:34 +0530

    fix: update donor count

--- src/components/Donation/components/DonateForm.js
+++ src/components/Donation/components/DonateForm.js
@@ -122,7 +122,7 @@ class DonateForm extends PureComponent {
             But it costs our nonprofit a lot of money to run all this.
           </p>
           <p>
-            Join the <strong>4,102</strong> people who donate $5 / month.
+            Join the <strong>3,926</strong> people who donate $5 / month.
           </p>
           <p>
             Let's build this community together.
