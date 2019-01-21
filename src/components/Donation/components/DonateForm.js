c64b1b30fc5a96b17eda7665b7e3094d23b51a42
mrugesh mohapatra <1884376+raisedadead@users.noreply.github.com>
2019-01-21 13:41:18 +0530

    fix: update donate stats - Jan 21, 2019

--- src/components/Donation/components/DonateForm.js
+++ src/components/Donation/components/DonateForm.js
@@ -117,7 +117,7 @@ class DonateForm extends PureComponent {
         <div className='text-center'>
           <p>
             freeCodeCamp.org is a tiny nonprofit that's helping millions
-            of people learn to code for free. Join <strong>4,116</strong> supporters.
+            of people learn to code for free. Join <strong>4,180</strong> supporters.
             Your $5 / month donation will help keep tech education
             free and open.
           </p>
